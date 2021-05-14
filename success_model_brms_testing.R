30b7fcefae575bb458e363b7faff2fa2311438f0
Ellen Brandell <ebrandell08@gmail.com>
2021-05-14 14:26:47 -0500

    creating new script to start modeling

---
+++ success_model_brms_testing.R
@@ -0,0 +1,14 @@
+## Ellen Brandell
+## May 2021
+
+
+# GOAL: initial modeling for *genotype success model* in Bayesian framework
+
+# load libraries for analysis
+library(brms)
+
+# load libraries for plotting
+library(tidyverse)
+library(viridis)
+library(cowplot)
+
