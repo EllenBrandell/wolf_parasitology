641801a9b27ca72cf72bf5aa3f7ff3a57bd5e4d5
Ellen Brandell <ebrandell08@gmail.com>
2021-05-18 15:34:43 -0500

    making goal notes

--- TPRS.R
+++ TPRS.R
@@ -1,4 +1,5 @@
 ## Ellen Brandell
 ## May 2021
 
-## Code for interpolating weather data across the study area using thin plate regression splines
+## Code for interpolating weather data across the study area using thin plate regression splines;
+## Daily weather data at each scat location is compiled and used as explanatory variables in the 'success' model.
