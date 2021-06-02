5102c22de28110e7717b5eef433e8eba36635158
Ellen Brandell <ebrandell08@gmail.com>
2021-06-02 11:55:32 -0500

    making diameter plots

--- age_cleaning.R
+++ age_cleaning.R
@@ -21,12 +21,10 @@ location <- read.csv("locations.csv")
 data1 <- merge(geno, diameter, by="scatID", all.x=T)
 # data1 <- merge(data1, meta[,c(1,4)], by="scatID", all.x=T) # I want site
 
-###### make a data set of JUST KNOWN WOLVES
-wolves1 <- data1[!is.na(data1$wolfID),]
-
 
 ######################################## DIAMETER PATTERNS
 ggplot(data=data1, aes(x=scat_diameter)) + geom_histogram(fill='gray', color='black')
+
 
 ## look at different diameter measurements by unique wolves
 
@@ -40,6 +38,8 @@ ggplot(data=data1, aes(x=scat_diameter)) + geom_histogram(fill='gray', color='bl
 cut <- 2.0  # less than OR equal to this is a pup
 data1$age <- ifelse(data1$scat_diameter<=cut, "Pup", "Adult")
 # make sure age isn't changing unless the wolf ages from Juv -> Adult
+ggplot(data=data1, aes(x=scat_diameter)) + geom_histogram(fill='gray', color='black') +
+  geom_vline(xintercept=cut, col='red', size=1.3)
 
 
 table(data1$age)
@@ -47,6 +47,40 @@ table(data1$age)/nrow(data1)
 # "questionable"/"in between" scat diameters based on our cutoff
 table(data1$scat_diameter>cut-0.3 & data1$scat_diameter<cut+0.3)
 # there are quite a few of these "in between" sizes
+
+
+###### make a data set of JUST KNOWN WOLVES
+wolves1 <- data1[!is.na(data1$wolfID),]
+
+wolf.sum <- wolves1 %>% group_by(wolfID) %>% summarize(sex=sex[1], ages=length(unique(age)), age=age[1], 
+                                                       pack=pack[1],
+                                                     e.c.=max(e.c.),e.m.=max(e.m.),t.m.d.=max(t.m.d.),
+                                                     sites=length(unique(indiv.site)), nsamples=n())
+table(wolf.sum$age)
+table(wolf.sum$age)/nrow(wolf.sum)
+
+table(wolf.sum$pack, wolf.sum$age)
+
+a <- ggplot(data=wolves1, aes(x=scat_diameter)) + geom_histogram(fill='gray', color='black') +
+  geom_vline(xintercept=cut, col='red', size=1.3) + xlab("scat diameter") + ggtitle("Scat Diameter Known Wolves")
+a
+b <- ggplot(data=wolves1, aes(y=scat_diameter, x=wolfID, color=wolfID)) + geom_point() + 
+  ylab("scat diameter") + ggtitle("Scat Diameter Known Wolves") + 
+  theme(axis.text.x=element_text(angle=90), legend.position="none") +
+  geom_hline(yintercept=cut, col='red', size=0.5)
+b
+c <- ggplot(data=wolves1, aes(y=scat_diameter, x=age, color=age)) + geom_boxplot() + geom_jitter(width=0.1) +
+  ylab("scat diameter") + ggtitle("Scat Diameter Known Wolves") +
+  geom_hline(yintercept=cut, col='red', size=0.7)
+c
+d <- ggplot(data=wolves1, aes(y=scat_diameter, x=sex, color=sex)) + geom_boxplot() + geom_jitter(width=0.2) +
+  ylab("scat diameter") + ggtitle("Scat Diameter Known Wolves") + 
+  geom_hline(yintercept=cut, col='red', size=0.7)
+d
+
+plot_grid(a,b,c,d, nrow=2, ncol=2)
+ggsave("scat_diameter.png", height=11, width=11, units="in", dpi=250)
+
 
 ###### HOW MANY MISMATCHES DO WE HAVE BASED ON CUTOFF VALUE?
 # can only use resampled wolves for this
