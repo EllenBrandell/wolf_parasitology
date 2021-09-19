060887d488318bf341b1f0429d5a71dad0a8a1ed
Ellen Brandell <ebrandell08@gmail.com>
2021-09-19 14:08:11 -0500

    working on model and code

--- SUCCESS_MODEL_CODE.R
+++ SUCCESS_MODEL_CODE.R
@@ -8,7 +8,7 @@ library(brms)      # Bayesian model
 library(tidybayes) # for analysis of posterior draws of a Bayesian model
 # load libraries for plotting
 library(tidyverse)
-library(viridis)
+# library(viridis)
 library(cowplot)
 library(bayesplot) # for plotting posterior draws of a Bayesian model
 
@@ -44,9 +44,135 @@ full.mod <- brm(formula= geno ~ period + cover + high_temp_mean + days_elapsed +
                 prior=prior.list,
                 warmup=nburn, iter=niter, chains=nchains)
 summary(full.mod)
+
+
+################################### ASSESS MODEL CONVERGENCE
+mcmc_plot(full.mod, type="neff_hist")    # effective sample size
+# ratio: Neff/N -> closer to 1 is better (lighter); light: between 0.5 and 1 (high); mid: between 0.1 and 0.5 (good); dark: below 0.1 (low)
+
+mcmc_plot(full.mod, type="rhat")         # R hat (~1)
+mcmc_plot(full.mod, type="rhat_hist")    # R hat (~1)
+# pull out specific rhat estimates of interest
+rhat(full.mod)
+
+mcmc_plot(full.mod, type="trace")        # trace plots
+mcmc_plot(full.mod, type="acf_bar")      # look at autocorrelation
+mcmc_plot(full.mod, type="dens_overlay") # density plots for each chain
+
+
+################################### PARAMETER ESTIMATES
 posterior_summary(full.mod, probs=c(.025, .25, .75, .975))
-plot(full.mod)
+posterior <- as.array(full.mod)
+dim(posterior) # iterations retained, chains, parameters
+dimnames(posterior)
+
+color_scheme_set("red")
+# line plots
+mcmc_intervals(posterior, pars=c("b_Intercept","b_days_elapsed","b_cover1","b_high_temp_mean","b_total_precip","b_periodwinter","b_periodz_den","sd_site__Intercept"))
+# density plots
+mcmc_areas(
+  posterior, 
+  pars=c("b_Intercept","b_days_elapsed","b_cover1","b_high_temp_mean","b_total_precip","b_periodwinter","b_periodz_den","sd_site__Intercept"),
+  prob=0.5, # 50% intervals
+  prob_outer=0.95, # 95%  intervals
+  point_est="mean"
+)
+# histograms
+mcmc_hist(posterior, pars=c("b_Intercept","b_days_elapsed","b_cover1","b_high_temp_mean","b_total_precip","b_periodwinter","b_periodz_den","sd_site__Intercept"))
+
+# manual plot of line estimate + density with shading
+post <- posterior_samples(full.mod, add_chain=T)
+post %>% 
+  ggplot(aes(x=b_Intercept, y=0)) +
+  stat_halfeye(point_interval=mode_hdi, .width=c(.95, .5)) +
+  scale_y_continuous(NULL, breaks=NULL) +
+  labs(title="Intercept",
+       x    =expression(beta)) +
+  theme(panel.grid=element_blank()) +
+  vline_0()
+post %>% 
+  ggplot(aes(x=b_cover1, y=0)) +
+  stat_halfeye(point_interval=mode_hdi, .width=c(.95, .5)) +
+  scale_y_continuous(NULL, breaks=NULL) +
+  labs(title="Covered",
+       x    =expression(beta)) +
+  theme(panel.grid=element_blank()) +
+  vline_0()
+post %>% 
+  ggplot(aes(x=b_high_temp_mean, y=0)) +
+  stat_halfeye(point_interval=mode_hdi, .width=c(.95, .5)) +
+  scale_y_continuous(NULL, breaks=NULL) +
+  labs(title="Mean high temperature (F)",
+       x    =expression(beta)) +
+  theme(panel.grid=element_blank()) +
+  vline_0()
+post %>% 
+  ggplot(aes(x=b_days_elapsed, y=0)) +
+  stat_halfeye(point_interval=mode_hdi, .width=c(.95, .5)) +
+  scale_y_continuous(NULL, breaks=NULL) +
+  labs(title="Days elapsed",
+       x    =expression(beta)) +
+  theme(panel.grid=element_blank()) +
+  vline_0()
+post %>% 
+  ggplot(aes(x=b_total_precip, y=0)) +
+  stat_halfeye(point_interval=mode_hdi, .width=c(.95, .5)) +
+  scale_y_continuous(NULL, breaks=NULL) +
+  labs(title="Total precipitation (inches)",
+       x    =expression(beta)) +
+  theme(panel.grid=element_blank()) +
+  vline_0()
+post %>% 
+  ggplot(aes(x=b_periodwinter, y=0)) +
+  stat_halfeye(point_interval=mode_hdi, .width=c(.95, .5)) +
+  scale_y_continuous(NULL, breaks=NULL) +
+  labs(title="Winter collection",
+       x    =expression(beta)) +
+  theme(panel.grid=element_blank()) +
+  vline_0()
+post %>% 
+  ggplot(aes(x=b_periodz_den, y=0)) +
+  stat_halfeye(point_interval=mode_hdi, .width=c(.95, .5)) +
+  scale_y_continuous(NULL, breaks=NULL) +
+  labs(title="Den collection",
+       x    =expression(beta)) +
+  theme(panel.grid=element_blank()) +
+  vline_0()
 
 
+############################################# EXPONENTIATING = ODDS
+# https://www.rensvandeschoot.com/tutorials/generalised-linear-models-with-brms/
+exp(fixef(full.mod))
+mcmc_plot(full.mod, 
+          type="areas",
+          prob=0.80,
+          transformations="exp") +
+  geom_vline(xintercept=1, color="grey") +
+  xlim(0,50)
 
+p1 <- mcmc_intervals(posterior, transformations="exp", 
+                     prob=0.5, prob_outer=0.7, inner_size=1, point_size=3,
+               pars=c("b_days_elapsed","b_cover1","b_high_temp_mean","b_total_precip"))
+p2 <- mcmc_intervals(posterior, transformations="exp", 
+                     prob=0.5, prob_outer=0.5, inner_size=1, point_size=3,
+                     pars=c("b_periodwinter","b_periodz_den"),)
+
+p1.1 <- p1 + scale_y_discrete(
+  labels=c("exp(b_days_elapsed)"="DAYS",
+           "exp(b_cover1)"="COVER (1)",
+           "exp(b_high_temp_mean)"="HIGH TEMP",
+           "exp(b_total_precip)"="PRECIP",
+           "exp(b_periodwinter)"="WINTER",
+           "exp(b_periodz_den)"="DEN" )  ) + 
+  xlim(0,3) + xlab("log-odds") + geom_vline(aes(xintercept=1),color="gray60")
+p1.1
+
+p2.1 <- p2 + scale_y_discrete(
+  labels=c("exp(b_periodwinter)"="WINTER",
+           "exp(b_periodz_den)"="DEN" )  ) + 
+  xlim(0,45) + xlab("log-odds") + geom_vline(aes(xintercept=1),color="gray60")
+p2.1
+
+plot_grid(p1.1,p2.1, nrow=1, labels=c("(A)","(B)"))
+ggsave("SUCCESS_MODEL_coef_plots.jpeg", height=5, width=10, units="in", dpi=300)
 
